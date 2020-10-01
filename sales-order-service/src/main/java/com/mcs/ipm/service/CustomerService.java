e5b264fc165c97478e050858b5173e4b53f4ed76
Deena <deenuurfriend@gmail.com>
2020-10-01 09:58:34 +0800

    initial commit

--- sales-order-service/src/main/java/com/mcs/ipm/service/CustomerService.java
+++ sales-order-service/src/main/java/com/mcs/ipm/service/CustomerService.java
@@ -1,12 +1,14 @@
 package com.mcs.ipm.service;
 
 import com.mcs.ipm.entity.Customer;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
 @FeignClient(name = "customer-service")
+@RibbonClient(name = "customer-service")
 public interface CustomerService {
 
     @GetMapping("/customers/{customerId}")
