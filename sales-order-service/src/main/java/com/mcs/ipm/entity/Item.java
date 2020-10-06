a0876be0fe5f2bff422f5599df1f8978041b3e7a
Deena <deenuurfriend@gmail.com>
2020-10-06 18:57:05 +0800

    Initial commit

--- sales-order-service/src/main/java/com/mcs/ipm/entity/Item.java
+++ sales-order-service/src/main/java/com/mcs/ipm/entity/Item.java
@@ -2,12 +2,14 @@ package com.mcs.ipm.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Item {
 
     private Long id;
